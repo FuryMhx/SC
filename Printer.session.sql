193b36e688be5f82827deb76322fca198c010e7d
FuriousPotato <106948234+FuryMhx@users.noreply.github.com>
2026-07-02 08:32:50 -0400

    update rules

--- Printer.session.sql
+++ Printer.session.sql
@@ -1,6 +1,3 @@
-SELECT 内容, count(*) as cnt FROM printer_almlist
-WHERE 时间::DATE = '2026-05-13' 
-  AND printer_id = 'SPT2.1_printer02'
-  AND (内容 ILIKE '%通讯超时%' OR 内容 ILIKE '% cotimeout%')
-GROUP BY 内容
-ORDER BY cnt DESC
+SELECT * FROM printer_almlist
+WHERE 时间::DATE = '2026-06-15' 
+AND printer_id = 'SPT2.1_printer01'
